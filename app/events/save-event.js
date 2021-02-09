a35964eeaa6d6d86241d21cde28c229cc291eb7c
John Watson <johnwatson484@hotmail.com>
2021-02-09 16:12:48 +0000

    Save event data

--- app/events/save-event.js
+++ app/events/save-event.js
@@ -1,7 +1,7 @@
 const { models } = require('../data')
 
 async function saveEvent (event) {
-  await models.event.create(event)
+  await models.event.create({ eventData: event })
   console.info(`Saved event, claimId: ${event.claimId}, type: ${event.type}`)
 }
 
